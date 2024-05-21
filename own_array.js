7e7d1783c820f9c53835f6cdfb798309764d5848
hoorrehan <167020018+hoorrehan@users.noreply.github.com>
2024-05-21 10:31:27 -0700

    Add files via upload

---
+++ own_array.js
@@ -0,0 +1,7 @@
+"use strict";
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+// to print a series of statements about these items, such as â€œI would like to own a Honda motorcycle.
+let transportation = ["car", "bike", "train", "plan"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log("I would like to own " + transportation[i]);
+}
