87788510c81996eacf5a12c2f5364e7e12716690
hoorrehan <167020018+hoorrehan@users.noreply.github.com>
2024-05-23 08:36:52 -0700

    Add files via upload

---
+++ city_name.ts
@@ -0,0 +1,16 @@
+// . City Names: Write a function called city_country() that takes in the name
+// of a city and its country. The function should return a string formatted like this:
+
+import { log } from "console";
+
+// "Lahore, Pakistan"
+
+// Call your function with at least three city-country pairs, and print the value
+// thatâ€™s returned.
+
+function city_country(city: string, country: string){
+console.log(`Hello world`);
+
+}
+    
+city_country('Karachi' , 'Pakistan');
